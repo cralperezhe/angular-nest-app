c4b5d50758c8942f91ad6c61ed1ecd5456d13164
cralperezhe <cralperezhe@ittepic.edu.mx>
2023-09-10 19:12:16 -0700

    Enrutamiento de las páginas y primera solicitud get de products.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +13,12 @@ import { ProductFormComponent } from './components/product-form/product-form.com
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    ProductFormComponent
+    ProductFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/product-list/product-list.component.ts
+++ client/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  
+  constructor(private productService: ProductService){}
 
+  ngOnInit(){
+    this.getProducts();
+  }
+  getProducts(){
+    this.productService.getProducts()
+    .subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err),
+      complete: () => console.log('complete')
+    })
+  }
 }
